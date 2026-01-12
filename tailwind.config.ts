862a2e305cc2ba3b2cce0943ecc7fc9e8bc3c8e5
William Chung <williamchung@ucsb.edu>
2026-01-11 20:13:19 -0800

    create global and tailwindcss styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
   	extend: {
   		colors: {
   			brand: {
-  				'100': '#EA6365',
-  				DEFAULT: '#FA7275'
+  				'100': '#63a9eaff',
+  				DEFAULT: '#72d1faff'
   			},
   			red: '#FF7474',
   			error: '#b80000',
