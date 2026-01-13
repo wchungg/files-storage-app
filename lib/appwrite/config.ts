a73b28b82fcdb61de7a942b2a7118a696370930b
William Chung <williamchung@ucsb.edu>
2026-01-13 00:20:19 -0800

    connect appwrite database and functions to create session

---
+++ lib/appwrite/config.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+    endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+    projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
+    usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
+    filesCollectionId: process.env.NEXT_PUBLIC_APPWRITE_FILES_COLLECTION!,
+    bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET!,
+    secretKey: process.env.NEXT_APPWRITE_KEY!,
+};
