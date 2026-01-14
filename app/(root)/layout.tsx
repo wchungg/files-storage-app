d79e44d1ffa3e3bac039a51fa3de49a832a9dc82
William Chung <williamchung@ucsb.edu>
2026-01-13 19:49:17 -0800

    create layout for components in root page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Header from '@/components/Header'
+import MobileNavigation from '@/components/MobileNavigation'
+import Sidebar from '@/components/Sidebar'
+import React from 'react'
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <main className='flex h-screen'>
+    <Sidebar />
+
+    <section className='flex h-full flex-1 flex-col'>
+        <MobileNavigation /> 
+        <Header />
+        <div className='main-content'>{children}</div>
+    </section>
+  </main>
+}
+
+export default layout
