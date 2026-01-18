9dbfff00b9a4ca0c6153e1262f1ed0280b5172b8
William Chung <williamchung@ucsb.edu>
2026-01-18 01:30:30 -0800

    implement card and actiondropdown components

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -2,6 +2,7 @@ import Sort from '@/components/Sort';
 import { getFiles } from '@/lib/actions/file.actions';
 import React from 'react'
 import { Models } from "node-appwrite";
+import Card from '@/components/Card';
 
 const Page = async ({ params }: SearchParamProps) => {
     const type = (await params)?.type as string || "";
@@ -29,7 +30,7 @@ const Page = async ({ params }: SearchParamProps) => {
     {files.total > 0 ? (
         <section className='file-list'>
             {files.documents.map((file: Models.Document) => (
-                <h1 key={file.$id} className='h1'>{file.name}</h1>
+                <Card key={file.$id} file={file} />
             ))}
         </section>
     ): (<p className='empty-list'>No files uploaded</p>)}
--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "cloud.appwrite.io",
+        hostname: "sfo.cloud.appwrite.io",
       },
     ],
   },
