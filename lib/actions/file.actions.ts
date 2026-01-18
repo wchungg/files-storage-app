8bbebbb52994f9e1ac627f3bb7993442c8cefce8
William Chung <williamchung@ucsb.edu>
2026-01-17 23:28:32 -0800

    implement get files from database functionality

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,11 +1,12 @@
 'use server'
 
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { InputFile } from "node-appwrite/file";
 import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { constructFileUrl, getFileType, parseStringify } from "../utils";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.actions";
 
 const handleError = (error: unknown, message: string) => {
     console.log(error, message);
@@ -53,3 +54,44 @@ export const uploadFile = async ({ file, ownerId, accountId, path }: UploadFileP
         handleError(error, "Failed to upload files")
     }
 };
+
+const createQueries = (currentUser: Models.Document) => {
+    const queries = [
+        Query.or([
+            Query.equal('owner', [currentUser.$id]),
+            Query.contains('users', [currentUser.email])
+        ])
+    ]
+
+    // TODO: Search, sort, limits...
+
+    return queries
+}
+
+export const getFiles = async () => {
+    const { databases } = await createAdminClient();
+
+    try {
+        const currentUser = await getCurrentUser()
+
+        if (!currentUser) throw new Error("User not found");
+
+        const queries = createQueries(currentUser)
+
+        console.log({ currentUser, queries})
+
+        const files = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.filesCollectionId,
+            queries,
+        )
+
+        console.log({ currentUser, queries})
+
+        return parseStringify(files);
+
+    } catch (error) {
+        handleError(error, "failed to get files")
+    }
+}
+
