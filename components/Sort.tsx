39ddbc9c7762a649d48c6b9ad086ffd918229888
William Chung <williamchung@ucsb.edu>
2026-01-20 10:54:05 -0800

    implement sort field component

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+'use client'
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+
 
 const Sort = () => {
+  const path = usePathname()
+  const router = useRouter();
+
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  }
+
   return (
-    <div>Sort</div>
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem key={sort.label} className="shad-select-item" value={sort.value}>
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   )
 }
 
