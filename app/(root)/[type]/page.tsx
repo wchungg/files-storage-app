4e6059940966e50faea89a610c3812f553d609c8
William Chung <williamchung@ucsb.edu>
2026-01-20 00:29:41 -0800

    implement search, sort, and result limit functionality

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -5,12 +5,14 @@ import { Models } from "node-appwrite";
 import Card from '@/components/Card';
 import { getFileTypesParams } from '@/lib/utils';
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
     const type = (await params)?.type as string || "";
+    const searchText = ((await searchParams)?.query as string) || "";
+    const sort = ((await searchParams)?.sort as string) || "";
 
     const types = getFileTypesParams(type) as FileType[];
     
-    const files = await getFiles({ types })
+    const files = await getFiles({ types, searchText, sort })
     
 
   return <div className="page-container">
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -55,7 +55,7 @@ export const uploadFile = async ({ file, ownerId, accountId, path }: UploadFileP
     }
 };
 
-const createQueries = (currentUser: Models.Document, types: string[]) => {
+const createQueries = (currentUser: Models.Document, types: string[], searchText: string, sort: string, limit?: number) => {
     const queries = [
         Query.or([
             Query.equal('owner', [currentUser.$id]),
@@ -64,11 +64,20 @@ const createQueries = (currentUser: Models.Document, types: string[]) => {
     ]
 
     if (types.length > 0) queries.push(Query.equal('type', types));
+    if (searchText) queries.push(Query.contains('name', searchText));
+    if (limit) queries.push(Query.limit(limit));
+
+    if (sort) {
+        const [sortBy, orderBy] = sort.split("-")
     
+        queries.push(orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy))
+    }
+
+
     return queries
 }
 
-export const getFiles = async ({ types = [] }: GetFilesProps) => {
+export const getFiles = async ({ types = [], searchText = '', sort = '$createdAt-desc', limit }: GetFilesProps) => {
     const { databases } = await createAdminClient();
 
     try {
@@ -76,7 +85,7 @@ export const getFiles = async ({ types = [] }: GetFilesProps) => {
 
         if (!currentUser) throw new Error("User not found");
 
-        const queries = createQueries(currentUser, types)
+        const queries = createQueries(currentUser, types, searchText, sort, limit)
 
         console.log({ currentUser, queries })
 
